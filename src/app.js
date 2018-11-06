46392b55a6fbb63890d6cb7a2a2f0cb037505a95
Ali CHERIFI <cherifiali@outlook.fr>
2018-11-06 17:34:57 +0100

    test husky

--- src/app.js
+++ src/app.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-  host: 'db', 
-  user:'root',
-  password: 'example', 
+  host: 'db',
+  user: 'root',
+  password: 'example',
   database: 'cdp',
   connectionLimit: 5});
 const app = express();
-
+var test = "";
 app.get('/', function(req, res) {
-    //res.send('Hello !');
-    pool.getConnection()
-    .then(conn => {
-
-      conn.query("SELECT * FROM `table` LIMIT 50")
-        .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        })
-        .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-          conn.end();
-        })
-        .catch(err => {
-          //handle error
-          console.log(err);
-          conn.end();
-        })
-
-    }).catch(err => {
-      //not connected
-      console.log(err);
-    });
-
+  // res.send('Hello !');
+  pool.getConnection()
+      .then((conn) => {
+        conn.query('SELECT * FROM `table` LIMIT 50')
+            .then((rows) => {
+              console.log(rows); // [ {val: 1}, meta: ... ]
+            })
+            .then((res) => {
+              conn.end();
+            })
+            .catch((err) => {
+              // handle error
+              console.log(err);
+              conn.end();
+            });
+      }).catch((err) => {
+      // not connected
+        console.log(err);
+      });
 });
 
 app.get('/home', function(req, res) {
@@ -43,7 +38,7 @@ app.get('/user/:uid', function(req, res) {
   res.send('Bonjour utilisateur ' + req.params.uid);
 });
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.status(404).send('Page introuvable !');
 });
 
