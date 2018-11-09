fb9aeaff07d5af75dc4e12eb305effac7a5841df
Ali CHERIFI <cherifiali@outlook.fr>
2018-11-09 15:19:24 +0100

    Suppression commentaire

--- src/public/client.js
+++ src/public/client.js
@@ -5,5 +5,4 @@ const button = document.getElementById('sub');
 button.addEventListener('click', function(e) {
   name = document.getElementById('name').value;
   psw = document.getElementById('psw').value;
-  console.log('oui' + name);
 });
