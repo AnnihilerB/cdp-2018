889eea6bad0b030baf2f5bb911c6f68ac1348504
Ali CHERIFI <cherifiali@outlook.fr>
2018-11-12 08:56:24 +0100

    Correction ESLint

--- src/__tests__/e2e/e2e.js
+++ src/__tests__/e2e/e2e.js
@@ -1,37 +1,22 @@
-/*const {Builder, By, Key} = require('selenium-webdriver');
-
-(async function example() {
-  const driver = await new Builder().forBrowser('safari').build();
-  try {
-    await driver.get('http://localhost:3000');
-    await driver.findElement(By.id('name')).sendKeys('admin', Key.RETURN);
-    await driver.findElement(By.id('psw')).sendKeys('pass', Key.RETURN);
-    await driver.sleep(1000);
-    await driver.findElement(By.tagName('h1'));
-  } finally {
-    await driver.quit();
-  }
-})();*/
-
-module.exports = { 
+module.exports = {
   'Authentication succes'(client) {
     client
-      .url('http://webappcdp:3000/')
-      .waitForElementVisible('body')
-      .setValue('#name', 'admin')
-      .setValue('#psw', 'admin')
-      .click('#sub')
-      .assert.containsText('h1', 'Bienvenue')
-      .end();
-  },'Authentication failure'(client) {
+        .url('http://webappcdp:3000/')
+        .waitForElementVisible('body', 1000)
+        .setValue('#name', 'admin')
+        .setValue('#psw', 'pass')
+        .click('#sub')
+        .assert.containsText('h1', 'Bienvenue')
+        .end();
+  }, 'Authentication failure'(client) {
     client
-      .url('http://webappcdp:3000/')
-      .waitForElementVisible('body')
-      .setValue('#name', 'admin')
-      .setValue('#psw', 'false')
-      .waitForElementVisible('#sub')
-      .click('#sub')
-      .assert.containsText('h1', 'Incorrect credentials')
-      .end();
-  }
+        .url('http://webappcdp:3000/')
+        .waitForElementVisible('body', 1000)
+        .setValue('#name', 'admin')
+        .setValue('#psw', 'false')
+        .waitForElementVisible('#sub', 1000)
+        .click('#sub')
+        .assert.containsText('h1', 'Incorrect credentials')
+        .end();
+  },
 };
