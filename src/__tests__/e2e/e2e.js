8f00f0cc8a60140ff89eb91ab56c37df70f475fb
Ali CHERIFI <cherifiali@outlook.fr>
2018-11-19 16:40:41 +0100

    Test puppeteer

--- src/__tests__/e2e/e2e.js
+++ src/__tests__/e2e/e2e.js
@@ -1,73 +1,9 @@
-module.exports = {
-  'Authentication succes'(client) {
-    client
-        .url('http://localhost:3000/')
-        .waitForElementVisible('body', 1000)
-        .setValue('#name', 'admin')
-        .setValue('#psw', 'admin')
-        .click('#sub')
-        .assert.containsText('h1', 'Bienvenue')
-        .end();
-  }, 'Authentication failure'(client) {
-    client
-        .url('http://localhost:3000/')
-        .waitForElementVisible('body', 1000)
-        .setValue('#name', 'admin')
-        .setValue('#psw', 'false')
-        .waitForElementVisible('#sub', 1000)
-        .click('#sub')
-        .assert.containsText('h1', 'Incorrect credentials')
-        .end();
-  }, 'Project added'(client) {
-    client
-        .url('http://localhost:3000/home/projects')
-        .waitForElementVisible('body', 1000)
-        .setValue('#project_id', 'test')
-        .setValue('#project_name', 'test')
-        .setValue('#project_description', 'test')
-        .setValue('#project_sprint_duration', 'test')
-        .setValue('#project_deposit_url', 'test')
-        .waitForElementVisible('#sub', 1000)
-        .click('#sub')
-        .assert.containsText('h1', 'Project added successfully')
-        .end();
-  }, 'Sprint added'(client) {
-    client
-        .url('http://localhost:3000/home/sprints')
-        .waitForElementVisible('body', 1000)
-        .setValue('#sprint_id', 'test')
-        .setValue('#sprint_name', 'test')
-        .setValue('#sprint_state', 'test')
-        .setValue('#sprint_project_id', 'test')
-        .waitForElementVisible('#sub', 1000)
-        .click('#sub')
-        .assert.containsText('h1', 'Sprint added successfully')
-        .end();
-  }, 'Task added'(client) {
-    client
-        .url('http://localhost:3000/home/tasks')
-        .waitForElementVisible('body', 1000)
-        .setValue('#task_id', 'test')
-        .setValue('#task_name', 'test')
-        .setValue('#task_sprint_id', 'test')
-        .setValue('#task_issue_id', 'test')
-        .waitForElementVisible('#sub', 1000)
-        .click('#sub')
-        .assert.containsText('h1', 'Task added successfully')
-        .end();
-  }, 'issue added'(client) {
-    client
-        .url('http://localhost:3000/home/issues')
-        .waitForElementVisible('body', 1000)
-        .setValue('#issue_id', 'test')
-        .setValue('#issue_description', 'test')
-        .setValue('#issue_state', 'test')
-        .setValue('#issue_difficulty', 'test')
-        .setValue('#issue_priority', 'test')
-        .setValue('#issue_project_id', 'test')
-        .waitForElementVisible('#sub', 1000)
-        .click('#sub')
-        .assert.containsText('h1', 'Issue added successfully')
-        .end();
-  },
-};
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://webappcdp:3000');
+  await page.screenshot({path: 'example.png'});
+  await browser.close();
+})();
