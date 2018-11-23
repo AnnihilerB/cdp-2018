a9809229f2d6ec5721e5a0f9f37445d530004419
Ali CHERIFI <cherifiali@outlook.fr>
2018-11-23 10:57:20 +0100

    Test E2E création de compte

--- src/__tests__/e2e.js
+++ src/__tests__/e2e.js
@@ -48,6 +48,27 @@ describe('Sprint creation successful', () => {
   });
 });
 
+describe('Account creation successful', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:3000/');
+  });
+
+  it('should create the user correctly', async () => {
+    await waitForPage('a#new');
+    await page.click('input#name');
+    await page.keyboard.type('jean');
+    await page.click('input#psw');
+    await page.keyboard.type('pass');
+    await page.click('input#psw2');
+    await page.keyboard.type('pass');
+    await page.click('input#email');
+    await page.keyboard.type('a@a.com');
+    await waitForPage('button#sub');
+    const url = page.url();
+    expect(url).toBe('http://localhost:3000/home');
+  });
+});
+
 /**
  * Click on a page element that redirects and wait for the
  * redirection to be completed.
