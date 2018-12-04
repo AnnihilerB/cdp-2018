7f66a1eef45ca5926b324dfb48b5a8e1575fc7c1
Ali CHERIFI <cherifiali@outlook.fr>
2018-12-04 17:31:03 +0100

    Passage en async await

--- src/app.js
+++ src/app.js
@@ -17,96 +17,91 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/user/login', function(req, res) {
+app.post('/user/login', async function(req, res) {
   const username = req.body.uname;
   const pass = req.body.psw;
-  userDAO.logUser(username, pass).then((connected) => {
-    if (connected) {
-      res.redirect('/home');
-    } else {
-      res.send('<h1>Incorrect credentials</h1>');
-    }
-  });
+  const connected = await userDAO.logUser(username, pass);
+  if (connected) {
+    res.redirect('/home');
+  } else {
+    res.send('<h1>Incorrect credentials</h1>');
+  }
 });
 
-app.post('/user/create', function(req, res) {
+app.post('/user/create', async function(req, res) {
   const username = req.body.uname;
   const psw1 = req.body.psw1;
   const psw2 = req.body.psw2;
   const mail = req.body.email;
   assert.strictEqual(psw1, psw2);
-  userDAO.userAlreadyExists(username).then((alreadyExists) => {
-    if (!alreadyExists) {
-      userDAO.createUser(username, psw1, mail).then(() => {
-        userDAO.logUser(username, psw1).then((connected) => {
-          res.redirect('/home');
-        });
-      });
-    } else {
-      res.send('<p>User already exists</p>');
+
+  const alreadyExists = await userDAO.userAlreadyExists(username);
+  console.log('already exists', alreadyExists);
+  if (!alreadyExists) {
+    await userDAO.createUser(username, psw1, mail);
+    console.log('create user done');
+    const connected = await userDAO.logUser(username, psw1);
+    console.log('connected', connected);
+    if (connected) {
+      res.redirect('/home');
     }
-  });
+  } else {
+    res.send('<p>User already exists.</p>');
+  }
 });
 
 
-app.get('/projects', function(req, res) {
-  JSDOM.fromFile('src/public/category.html').then((dom) => {
-    const doc = dom.window.document;
-    projectDAO.getProjects().then((projects) => {
-      if (projects === undefined) {
-        res.send('<p>No projects yet.</p>');
-      } else {
-        const list = doc.querySelector('.list-group');
-        list.innerHTML = renderer.renderProjectList(projects);
-        doc.querySelector('#creationButton').href = '/projects/add';
-        doc.querySelector('.btn').innerHTML = 'Créer projet';
-        res.send(dom.serialize());
-      }
-    });
-  });
-});
-
-app.get('/projects/add', function(req, res) {
-  JSDOM.fromFile('src/public/form.html').then((dom) => {
-    const form = dom.window.document.querySelector('form');
-    form.method = 'POST';
-    form.action = '/projects/add';
-    form.innerHTML = renderer.renderProjectForm();
-    dom.window.document.querySelector('.navbar-brand').innerHTML = 'Créer un projet';
+app.get('/projects', async function(req, res) {
+  const dom = await JSDOM.fromFile('src/public/category.html');
+  const doc = dom.window.document;
+  const projects = await projectDAO.getProjects();
+  if (projects === undefined) {
+    res.send('<p>No projects yet.</p>');
+  } else {
+    const list = doc.querySelector('.list-group');
+    list.innerHTML = renderer.renderProjectList(projects);
+    doc.querySelector('#creationButton').href = '/projects/add';
+    doc.querySelector('.btn').innerHTML = 'Créer projet';
     res.send(dom.serialize());
-  });
+  }
 });
 
-app.post('/projects/add', function(req, res) {
-  console.log(req.body);
+app.get('/projects/add', async function(req, res) {
+  const dom = await JSDOM.fromFile('src/public/form.html');
+  const form = dom.window.document.querySelector('form');
+  form.method = 'POST';
+  form.action = '/projects/add';
+  form.innerHTML = renderer.renderProjectForm();
+  dom.window.document.querySelector('.navbar-brand').innerHTML = 'Créer un projet';
+  res.send(dom.serialize());
+});
+
+app.post('/projects/add', async function(req, res) {
   const projectName = req.body.project;
   const sprintDuration = req.body.sprint_duration;
-  projectDAO.createProject(projectName, sprintDuration).then(()=>{
-    res.send('<p>Project Created</p>');
-  });
+  await projectDAO.createProject(projectName, sprintDuration);
+  res.redirect('/projects');
 });
 
-app.get('/sprint', function(req, res) {
-  JSDOM.fromFile('src/public/form.html').then((dom) => {
-    const form = dom.window.document.querySelector('form');
-    form.action = '/sprint/add';
-    form.innerHTML = renderer.renderSprintForm();
-    dom.window.document.querySelector('.navbar-brand').innerHTML = 'Créer un sprint';
-    res.send(dom.serialize());
-  });
+app.get('/sprint', async function(req, res) {
+  const dom = await JSDOM.fromFile('src/public/form.html');
+  const form = dom.window.document.querySelector('form');
+  form.action = '/sprint/add';
+  form.innerHTML = renderer.renderSprintForm();
+  dom.window.document.querySelector('.navbar-brand').innerHTML = 'Créer un sprint';
+  res.send(dom.serialize());
 });
 
-app.post('/sprint/add', function(req, res) {
+app.post('/sprint/add', async function(req, res) {
   const sprintName = req.body.sprint;
   const sprintState = req.body.sprint_state;
   const projectID = req.body.sprint_projectid;
-  sprintDAO.createSprint(sprintName, sprintState, projectID).then((isCreated)=>{
-    if (isCreated) {
-      res.send('<p>Sprint Created</p>');
-    } else {
-      res.send('An error has occured');
-    }
-  });
+  const isCreated = await sprintDAO.createSprint(sprintName, sprintState, projectID);
+  if (isCreated) {
+    res.send('<p>Sprint Created</p>');
+  } else {
+    res.send('An error has occured');
+  }
 });
 
 app.get('/home', function(req, res) {
