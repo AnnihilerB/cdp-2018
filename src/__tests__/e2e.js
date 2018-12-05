70c191f75b47ea990c82b85a8227807367c77bfa
Ali CHERIFI <cherifiali@outlook.fr>
2018-12-05 11:34:20 +0100

    MAJ des tests

--- src/__tests__/e2e.js
+++ src/__tests__/e2e.js
@@ -1,7 +1,36 @@
 jest.setTimeout(30000);
+const puppeteer = require('puppeteer');
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'example',
+  database: 'cdp',
+  connectionLimit: 5});
+
+let browser; let page;
+
+/**
+ * Prepare the browser for the test
+ */
+async function prep() {
+  browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ], headless: true});
+  page = await browser.newPage();
+}
 
 describe('Login user', () => {
   beforeAll(async () => {
+    await prep();
+    const conn = await pool.getConnection();
+    await conn.query('SET FOREIGN_KEY_CHECKS = 0; ');
+    await conn.query(`TRUNCATE TABLE sprints;`);
+    await conn.query(`TRUNCATE TABLE projects;`);
+    await conn.query(`DELETE FROM users WHERE username <>'admin';`);
+    conn.end();
     await page.goto('http://localhost:3000');
   });
 
@@ -13,31 +42,37 @@ describe('Login user', () => {
     await waitForPage('button#sub');
     const url = page.url();
     expect(url).toBe('http://localhost:3000/home');
+    browser.close();
   });
 });
 
 describe('Project creation successful', () => {
   beforeAll(async () => {
+    await prep();
     await page.goto('http://localhost:3000/home');
   });
 
-  it('should log the user correctly', async () => {
-    await waitForPage('a#createProject');
+  it('should create a project', async () => {
+    await waitForPage('a#projects');
+    await waitForPage('.btn');
     await page.click('input#name');
     await page.keyboard.type('Mon projet');
+    await page.click('input#weeks2');
     await waitForPage('button#sendProject');
     const url = page.url();
-    expect(url).toBe('http://localhost:3000/project/add');
+    expect(url).toBe('http://localhost:3000/projects');
+    browser.close();
   });
 });
 
 describe('Sprint creation successful', () => {
   beforeAll(async () => {
+    await prep();
     await page.goto('http://localhost:3000/home');
   });
 
-  it('should log the user correctly', async () => {
-    await waitForPage('a#createSprint');
+  it('should Create a sprint', async () => {
+    await waitForPage('a#sprints');
     await page.click('input#name');
     await page.keyboard.type('Mon sprint');
     await page.click('input#projectId');
@@ -45,15 +80,17 @@ describe('Sprint creation successful', () => {
     await waitForPage('button#sendSprint');
     const url = page.url();
     expect(url).toBe('http://localhost:3000/sprint/add');
+    browser.close();
   });
 });
 
 describe('Account creation successful', () => {
   beforeAll(async () => {
+    await prep();
     await page.goto('http://localhost:3000/');
   });
 
-  it('should create the user correctly', async () => {
+  it('should create an account and be logged', async () => {
     await waitForPage('a#new');
     await page.click('input#name');
     await page.keyboard.type('jean');
@@ -66,6 +103,7 @@ describe('Account creation successful', () => {
     await waitForPage('button#sub');
     const url = page.url();
     expect(url).toBe('http://localhost:3000/home');
+    browser.close();
   });
 });
 
---
+++ src/__tests__/integration/userDAO.test.js
@@ -0,0 +1,55 @@
+const userDAO = require('../../userDAO');
+const mariadb = require('mariadb');
+let testpool = null;
+
+beforeAll(async () => {
+  jest.setTimeout(10000);
+  testpool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'example',
+    database: 'cdp',
+    connectionLimit: 5});
+  userDAO.setPool(testpool);
+  const conn = await testpool.getConnection();
+  await conn.query('DELETE FROM sprints');
+  await conn.query(`DELETE FROM projects`);
+  await conn.query(`DELETE FROM users WHERE username <>'admin';`);
+  conn.end();
+});
+
+test('Should log the user ', async () => {
+  const connected = await userDAO.logUser('admin', 'pass');
+  expect(connected).toBe(true);
+});
+
+test('Wrong password', async () => {
+  const connected = await userDAO.logUser('admin', 'nope');
+  expect(connected).toBe(false);
+});
+
+test('Should create a new user', async () => {
+  const username = 'John';
+  const pass = 'pass';
+  const mail = 'a@a.com';
+  await userDAO.createUser(username, pass, mail);
+  const conn = await testpool.getConnection();
+  const rows = await conn.query(`SELECT * FROM users`);
+  conn.end();
+  // admin and new user
+  expect(rows.length).toBe(2);
+  expect(rows[1].username).toBe(username);
+  expect(rows[1].password).toBe(pass);
+  expect(rows[1].email).toBe(mail);
+});
+
+test('User should exist', async () => {
+  const res = await userDAO.userAlreadyExists('admin');
+  expect(res).toBe(true);
+});
+
+test('User should not exist', async () => {
+  const res = await userDAO.userAlreadyExists('Michael');
+  expect(res).toBe(false);
+});
+
